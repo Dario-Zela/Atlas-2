cf1d549c4d4906b4cab6a24a7c1baa0e4bd35b48
Dario Zela <dariozela1@gmail.com>
2022-03-14 21:28:01 +0000

    Started making the GUI for the Hub/Browser Implemented the initial display, and added user controls for new and existing projects

---
+++ Editor/Browser/Browser.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Editor.Browser
+{
+    /// <summary>
+    /// Interaction logic for Loader.xaml
+    /// </summary>
+    public partial class Browser : Window
+    {
+        public Browser()
+        {
+            InitializeComponent();
+            Projects.IsSelected = true;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Projects.IsSelected)
+            {
+                Existing_Projects.Visibility = Visibility.Visible;
+                New_Projects.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                New_Projects.Visibility = Visibility.Visible;
+                Existing_Projects.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
