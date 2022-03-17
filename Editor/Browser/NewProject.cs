8f7ad8f87ea07bc3b21e47b39f55ef172d65b3f2
Dario Zela <dariozela1@gmail.com>
2022-03-17 12:55:46 +0000

    Finished Creating New Project Behaviour

--- Editor/Browser/NewProject.cs
+++ Editor/Browser/NewProject.cs
@@ -32,6 +32,7 @@ namespace Editor.Browser
 
         private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\Atlas\Projects\";
         private string _projectName = "New Project";
+        private bool _isValid = true;
 
         public string ProjectName
         {
@@ -42,10 +43,10 @@ namespace Editor.Browser
                 {
                     _projectName = value;
                     OnProprietyChanged(nameof(ProjectName));
+                    ValidateProject();
                 }
             }
         }
-
         public string ProjectPath
         {
             get => _projectPath;
@@ -55,12 +56,96 @@ namespace Editor.Browser
                 {
                     _projectPath = value;
                     OnProprietyChanged(nameof(ProjectPath));
+                    ValidateProject();
+                }
+            }
+        }
+        public bool IsValid
+        {
+            get => _isValid;
+            set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnProprietyChanged(nameof(IsValid));
                 }
             }
         }
 
-        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        private readonly ObservableCollection<ProjectTemplate> _projectTemplates = new();
         public ReadOnlyCollection<ProjectTemplate> ProjectTemplates { get; }
+
+        private bool ValidateProject()
+        {
+            var path = ProjectPath;
+            if(!Path.EndsInDirectorySeparator(path))
+                path += @"\";
+
+            path += $@"{ProjectName}\";
+
+            IsValid = false;
+
+            if (string.IsNullOrEmpty(ProjectName.Trim()))
+            {
+                return false;
+            }
+            else if (ProjectName.IndexOfAny(Path.GetInvalidFileNameChars()) != -1)
+            {
+                return false;
+            }
+            else if (string.IsNullOrEmpty(ProjectPath.Trim()))
+            {
+                return false;
+            }
+            else if (ProjectPath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                return false;
+            }
+            else if (Directory.Exists(path) && Directory.EnumerateFileSystemEntries(path).Any())
+            {
+                return false;
+            }
+
+            IsValid = true;
+            return true;
+        }
+
+        public string CreateProject(ProjectTemplate projectTemplate)
+        {
+            if (!ValidateProject()) 
+                return string.Empty;
+
+            var path = ProjectPath;
+            if (!Path.EndsInDirectorySeparator(path))
+                path += @"\";
+
+            path += $@"{ProjectName}\";
+
+            try
+            {
+                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                foreach (var foulder in projectTemplate.Folders)
+                {
+                    Directory.CreateDirectory(Path.GetFullPath(Path.Combine(Path.GetDirectoryName(path) ?? "", foulder)));
+                }
+
+                var directory = new DirectoryInfo(path + ".Atlas");
+                directory.Attributes |= FileAttributes.Hidden;
+
+                var projectXML = File.ReadAllText(projectTemplate.ProjectFilePath);
+                projectXML = String.Format(projectXML, ProjectName, ProjectPath);
+                var projectPath = Path.GetFullPath(Path.Combine(path, $"{ProjectName}{Project.Extention}"));
+                File.WriteAllText(projectPath, projectXML);
+
+                return path;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return string.Empty;
+            }
+        }
 
         public NewProject()
         {
@@ -74,12 +159,16 @@ namespace Editor.Browser
                 foreach (var file in templates)
                 {
                     var template = Serialiser.FromFile<ProjectTemplate>(file);
-                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "icon.png"));
+                    string directory = Path.GetDirectoryName(file)!.ToString();
+
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(directory, "icon.png"));
                     template.Icon = File.ReadAllBytes(template.IconFilePath);
-                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(directory, template.ProjectFile));
 
                     _projectTemplates.Add(template);
                 }
+
+                ValidateProject();
             }
             catch (Exception ex)
             {
